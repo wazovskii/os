9fa61cc0168c0893086c571918de958199d9503f
Nizamova Veronika <wazovski@MacBook-Pro-Veronika.local>
2020-09-22 19:26:48 +0700

    added another net

--- lab1.sh
+++ lab1.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-mask=$(sudo ifconfig en0 | awk '/netmask/{print$4}')
-ip=$(ifconfig en0 | awk '/inet 1/{print $2}')
-gate=$(route -n get default | awk '/gateway/{print$2}')
-speed=$(ifconfig en0 | awk '/media/{print $2}')
-mac=$(ifconfig en0 | awk '/ether/{print $2}')
+maskwifi=$(sudo ifconfig en0 | awk '/netmask/{print$4}')
+ipwifi=$(ifconfig en0 | awk '/inet 1/{print $2}')
+gatewifi=$(route -n get default | awk '/gateway/{print$2}')
+speedwifi=$(ifconfig en0 | awk '/media/{print $2}')
+macwifi=$(ifconfig en0 | awk '/ether/{print $2}')
+maskblu=$(sudo ifconfig en3 | awk '/netmask/{print$4}')
+ipblu=$(ifconfig en3 | awk '/inet 1/{print $2}')
+gateblu=$(sudo netstat -nr | grep default | awk '/en3/{print$2}')
+speedblu=$(ifconfig en3 | awk '/media/{print $2}')
+macblu=$(ifconfig en3 | awk '/ether/{print $2}')
 echo
 while [ -n "$1" ]
 do
@@ -22,26 +27,32 @@ case "$1" in
 -l)
     echo " Network interfaces:"
     echo "    Wi-Fi"
-    echo "    MAC address: $mac"
-    echo "    IP: $ip"
-    echo "    Connection speed: $speed"
-    echo "    Mask: $mask"
-    echo "    Gateway: $gate"
+    echo "      MAC address: $macwifi"
+    echo "      IP: $ipwifi"
+    echo "      Connection speed: $speedwifi"
+    echo "      Mask: $maskwifi"
+    echo "      Gateway: $gatewifi"
+    echo "    Bluetooth tethering"
+    echo "      MAC address: $macblu"
+    echo "      IP: $ipblu"
+    echo "      Connection speed: $speedblu"
+    echo "      Mask: $maskblu"
+    echo "      Gateway: $gateblu"
 ;;
 -k)
-    sudo ifconfig en0 down
-    echo "Wi-Fi is disabled"
+    sudo ifconfig en3 down
+    echo "Tethering is disabled"
 ;;
 -r)
-    sudo ifconfig en0 up
-    echo "Wi-Fi is enabled"
+    sudo ifconfig en3 up
+    echo "Tethering is enabled"
 ;;
 -sip)
-    sudo ifconfig en0 $2
+    sudo ifconfig en3 $2
     echo "IP changed to $2"
 ;;
 -sm)
-        sudo ifconfig en0 $ip netmask $2
+        sudo ifconfig en3 $ipblu netmask $2
         echo "Mask changed to $2"
 ;;
 -sg)
