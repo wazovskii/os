ec2939f814739a89fc1da66d6f5235babd010196
Nizamova Veronika <veroni3ka2000@gmail.com>
2020-10-02 18:44:24 +0700

    menu added

--- lab2.c
+++ lab2.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <dirent.h>
 
 static unsigned int total = 0;
 
@@ -23,6 +24,22 @@ int size(int argc, char **argv) {
     }
     printf("%s: %u\n", argv[1], total);
     return 0;
+}
+
+int ls(int argn, char * argv[])
+{
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(argv[1]);
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    }
+    return(0);
 }
 
 void trans(int argn, char * argv[])
@@ -89,9 +106,35 @@ void cpy(int argn, char * argv[])
 }
 
 int main(int argn, char * argv[]) {
-    //trans(argn, argv);
-   // remove(argv[1]);
-   // cpy(argn,argv);
-    size(argn, argv);
+    if(strncmp(argv[1],"-h",2)==0)
+    {
+        printf("Copyright reserved by Nizamova Veronika\n");
+        printf("Avalible arguments are: \n");
+        printf("-t to transfer file to another directory example:\n  -t /Users/wazovski/Desktop/distant/oss/test.txt /Users/wazovski/Desktop/distant/tes \n");
+        printf("-r to delete file example:\n -r /Users/wazovski/Desktop/distant/oss/test.txt \n");
+        printf("-c to copy file in present directory example:\n /Users/wazovski/Desktop/distant/oss/test.txt \n");
+        printf("-s to see size of directory or file  example:\n /Users/wazovski/Desktop/distant/oss/test.txt \n");
+        printf("-ls to see all files in directory  example:\n /Users/wazovski/Desktop/distant/oss \n");
+    }
+    else if(strncmp(argv[1],"-t",2)==0)
+    {
+        trans(argn, argv);
+    }
+    else if(strncmp(argv[1],"-r",2)==0)
+    {
+        remove(argv[1]);
+    }
+    else if(strncmp(argv[1],"-c",2)==0)
+    {
+        cpy(argn,argv);
+    }
+    else if(strncmp(argv[1],"-s",2)==0)
+    {
+        size(argn, argv);
+    }
+    else if(strncmp(argv[1],"-ls",2)==0)
+    {
+        ls(argn,argv);
+    }
     
 }
